c84d50803470ad4a4acdf61d6bf865c3fffd1a7b
pmrinal <67900656+pmrinal@users.noreply.github.com>
2024-05-26 03:22:42 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+//let score = true
+//console.log(typeof(score));
+//console.log(typeof score);
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//console.log(typeof NaN);
+
+//let isLoggedIn = "hello"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let strNumber = String(someNumber)
+//console.log(typeof strNumber)
+console.log(strNumber)
+console.log(typeof strNumber)
